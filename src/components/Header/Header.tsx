071e453139d89addaa522d90130c59b86e65e499
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2023-12-13 11:52:16 +0200

    finish doing employees page functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,59 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Layout, Space, Typography } from "antd";
-import { Link } from "react-router-dom";
-import { LoginOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  LoginOutlined,
+  LogoutOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 import CustomButton from "../CustomButton";
 import { Paths } from "../../utils/paths";
+import { selectUser } from "../../features/auth/selectors";
+import { logout } from "../../features/auth/authSlice";
 
 import styles from "./header.module.css";
 
 const Header = () => {
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(logout());
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <Layout.Header className={styles.header}>
       <Space>
         <TeamOutlined className={styles.teamIcon} />
         <Link to={Paths.home}>
-          <CustomButton type='link'>
+          <CustomButton type="link">
             <Typography.Title level={1}>Employees</Typography.Title>
           </CustomButton>
         </Link>
       </Space>
-      <Space>
-        <Link to={Paths.register}>
-          <CustomButton icon={<UserOutlined />}>Register</CustomButton>
-        </Link>
-        <Link to={Paths.login}>
-          <CustomButton icon={<LoginOutlined />}>Login</CustomButton>
-        </Link>
-      </Space>
+      {user ? (
+        <CustomButton
+          type="ghost"
+          icon={<LogoutOutlined />}
+          onClick={handleLogOut}
+        >
+          Log out
+        </CustomButton>
+      ) : (
+        <Space>
+          <Link to={Paths.register}>
+            <CustomButton icon={<UserOutlined />}>Register</CustomButton>
+          </Link>
+          <Link to={Paths.login}>
+            <CustomButton icon={<LoginOutlined />}>Login</CustomButton>
+          </Link>
+        </Space>
+      )}
     </Layout.Header>
   );
 };
--- src/pages/employees/EmployeesPage.tsx
+++ src/pages/employees/EmployeesPage.tsx
@@ -1,11 +1,36 @@
 import { PlusCircleOutlined } from "@ant-design/icons";
 import { Table } from "antd";
+import { useSelector } from "react-redux";
 
 import CustomButton from "../../components/CustomButton";
-import { useGetAllEmployeesQuery } from "../../app/services/employess";
+import {
+  Employee,
+  useGetAllEmployeesQuery,
+} from "../../app/services/employess";
+import type { ColumnsType } from "antd/es/table";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "../../utils/paths";
+import { selectUser } from "../../features/auth/selectors";
+
+import styles from "./employees.module.css";
+import { useEffect } from "react";
+
+const columnsType: ColumnsType<Employee> = [
+  { title: "Name", dataIndex: "firstName", key: "firstName" },
+  { title: "Age", dataIndex: "age", key: "age" },
+  { title: "Address", dataIndex: "address", key: "address" },
+];
 
 const EmployeesPage = () => {
   const { data, isLoading } = useGetAllEmployeesQuery();
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [navigate, user]);
 
   return (
     <>
@@ -16,7 +41,17 @@ const EmployeesPage = () => {
       >
         Add employee
       </CustomButton>
-      <Table></Table>
+      <Table
+        loading={isLoading}
+        dataSource={data}
+        pagination={false}
+        columns={columnsType}
+        rowKey={(record) => record.id}
+        onRow={(record) => {
+          return { onClick: () => navigate(`${Paths.employee}/${record.id}`) };
+        }}
+        rowClassName={styles.row}
+      ></Table>
     </>
   );
 };
